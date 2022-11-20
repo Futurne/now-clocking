a57f4810c01735d283b19ba0f7ee9b55aafee6bf
Pierrot LC <pstmrtem@outlook.com>
2022-11-20 17:36:09 +0100

    feat: autostart

---
+++ autostart.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir -p "$HOME/.config/autostart"
+
+# Write default entries
+cat "./now-clocking.desktop" > "$HOME/.config/autostart/now-clocking.desktop"
+
+# Write execution path
+exec_path="$(pwd)/start.sh"
+echo "Exec=sh $exec_path" >> "$HOME/.config/autostart/now-clocking.desktop"
