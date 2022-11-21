5158b417cbe922699fa99309ff97618d8c847a60
Pierrot LC <pstmrtem@outlook.com>
2022-11-21 19:44:36 +0100

    feat: simplifying image fetching

--- conky/npart.lua
+++ conky/npart.lua
@@ -1,44 +1,55 @@
+local function add_conky(text)
+	conky.text = conky.text .. text
+end
+
+local function fetch_image(application_name)
+	local text = ""
+		.. "${exec ./scripts/fetch-art "
+		.. application_name
+		.. " }"
+		.. "${image ./data/"
+		.. application_name
+		.. ".png -p 0,0 -s 125x125 -n}"
+	add_conky(text)
+end
+
+local function add_player(application_name)
+	local text = "" .. '${if_match "Playing" == "${exec playerctl -p ' .. application_name .. ' status}"}'
+	add_conky(text)
+	fetch_image(application_name)
+	add_conky("${endif}")
+end
+
 conky.config = {
-    -- Run settings
-    total_run_times = 0,
-    update_interval = 1,
+	-- Run settings
+	total_run_times = 0,
+	update_interval = 1,
 
-    -- Positioning
-    alignment = 'bottom_left',
-    gap_x = 50,
-    gap_y = 125,
-    minimum_width = 125,
-    maximum_width = 125,
-    minimum_height = 125,
-    xinerama_head = 1,
+	-- Positioning
+	alignment = "bottom_left",
+	gap_x = 50,
+	gap_y = 125,
+	minimum_width = 125,
+	maximum_width = 125,
+	minimum_height = 125,
+	xinerama_head = 1,
 
-    -- Draw settings
-    double_buffer = true,
-    draw_shades = false,
-    draw_blended = false,
+	-- Draw settings
+	double_buffer = true,
+	draw_shades = false,
+	draw_blended = false,
 
-    -- Compositor settings
-    own_window = true,
-    own_window_argb_visual = true,
-    own_window_argb_value = 0,
-    own_window_transparent = true,
-    own_window_hints = 'undecorated,below,sticky,skip_taskbar,skip_pager',
-    own_window_type = 'dock'
-};
+	-- Compositor settings
+	own_window = true,
+	own_window_argb_visual = true,
+	own_window_argb_value = 0,
+	own_window_transparent = true,
+	own_window_hints = "undecorated,below,sticky,skip_taskbar,skip_pager",
+	own_window_type = "dock",
+}
 
-conky.text = [[
-${if_running spotify}${exec ./scripts/fetch-art spotify}
-	${image ./data/spotify.png -p 0,0 -s 125x125 -n}
-${else}${if_match "" != "${exec playerctl -p vlc status}"}${exec ./scripts/fetch-art vlc}
-	${image ./data/vlc.png -p 0,0 -s 125x125 -n}
-${else}${if_match "" != "${exec playerctl -p Lollypop status}"}${exec ./scripts/fetch-art Lollypop}
-    ${image ./data/Lollypop.png -p 0,0 -s 125x125 -n}
-${else}${if_match "" != "${exec playerctl -p spotifyd status}"}${exec ./scripts/fetch-art spotifyd}
-    ${image ./data/spotifyd.png -p 0,0 -s 125x125 -n}
-${else}${if_running cmus}${exec ./scripts/fetch-art cmus}
-	${image ./data/cmus.png -p 0,0 -s 125x125 -n}
-${endif}
-${endif}
-${endif}
-${endif}
-]];
+conky.text = ""
+local players = { "spotify", "spotifyd", "Lollypop", "cmus", "vlc" }
+for _, application_name in ipairs(players) do
+	add_player(application_name)
+end
